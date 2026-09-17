9064e1758e84de81497898e16a169ee3e37b6f19
axmensstore2020-droid <axmensstore2020@gmail.com>
2026-09-17 23:11:14 +0400

    Allow search crawlers on public storefront pages

--- app/robots.js
+++ app/robots.js
@@ -2,9 +2,14 @@ import {SITE_URL} from '../lib/seo';
 
 export default function robots(){
  const indexing=process.env.AX_ALLOW_INDEXING==='true';
+ const privatePaths=['/api/','/account','/account/'];
  return {
   rules:indexing
-   ? [{userAgent:'*',allow:'/',disallow:['/api/','/account','/account/']}]
+   ? [
+      {userAgent:'*',allow:'/',disallow:privatePaths},
+      // ChatGPT search discovery uses OAI-SearchBot. Keep commerce/account endpoints private.
+      {userAgent:'OAI-SearchBot',allow:'/',disallow:privatePaths}
+     ]
    : [{userAgent:'*',disallow:'/'}],
   ...(indexing?{sitemap:`${SITE_URL}/sitemap.xml`,host:SITE_URL}:{})
  };
