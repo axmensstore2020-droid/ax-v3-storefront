db73935b57bea93e096055ec10d342e3429b6405
axmensstore2020-droid <axmensstore2020@gmail.com>
2026-09-19 10:35:17 +0400

    Add saved pieces page

---
+++ app/wishlist/page.js
@@ -0,0 +1,10 @@
+import WishlistPageClient from '../../components/WishlistPageClient';
+
+export const metadata={
+  title:'Saved pieces',
+  description:'Your saved AX Men’s Store pieces.'
+};
+
+export default function WishlistPage(){
+  return <WishlistPageClient/>;
+}
