95e9b4e1c575a57d33c70a9293645e87ae42c20b
axmensstore2020-droid <axmensstore2020@gmail.com>
2026-09-21 19:05:59 +0400

    Test shared colour swatch mapping

---
+++ tests/color.test.js
@@ -0,0 +1,11 @@
+import test from 'node:test';
+import assert from 'node:assert/strict';
+import {swatchColor} from '../lib/color.js';
+
+test('merchandising colour names map to stable swatch colours',()=>{
+ assert.equal(swatchColor('Black'),'#151515');
+ assert.equal(swatchColor('Ivory'),'#f1ead8');
+ assert.equal(swatchColor('Washed Blue'),'#58739a');
+ assert.equal(swatchColor('Cream'),'#ddd1b9');
+ assert.equal(swatchColor('Unknown shade'),'#b8b8b2');
+});
