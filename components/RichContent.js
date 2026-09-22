5b14528f470c1ac8ec034c8b3dd4c250e1bba93c
axmensstore2020-droid <axmensstore2020@gmail.com>
2026-09-22 23:42:33 +0400

    Use audited rich content sanitizer

--- components/RichContent.js
+++ components/RichContent.js
@@ -1,9 +1,5 @@
-import sanitizeHtml from 'sanitize-html';
+import {sanitizeRichContent} from '../lib/sanitize-content.js';
 export default function RichContent({html}) {
- const safe=sanitizeHtml(html || '',{
-  allowedTags:['p','br','strong','b','em','i','u','s','h2','h3','h4','ul','ol','li','a','blockquote','div','span','table','thead','tbody','tr','th','td','hr'],
-  allowedAttributes:{a:['href','title'],th:['scope','colspan'],td:['colspan']},
-  allowedSchemes:['http','https','mailto'],allowProtocolRelative:false
- });
+ const safe=sanitizeRichContent(html);
  return <div className="rich-content" dangerouslySetInnerHTML={{__html:safe}}/>;
 }
