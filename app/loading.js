3fd2c5dff13c307b1bb20025317ba1c4e9c3862a
axmensstore2020-droid <axmensstore2020@gmail.com>
2026-09-22 23:36:21 +0400

    Keep route loader out of the main landmark

--- app/loading.js
+++ app/loading.js
@@ -1,7 +1,7 @@
 import AXParticleLoader from '../components/AXParticleLoader';
 
 export default function Loading(){
-  return <main id="main-content" className="ax-route-loading" aria-busy="true">
+  return <div className="ax-route-loading" aria-busy="true">
     <AXParticleLoader variant="route" label="Loading AX" />
-  </main>;
+  </div>;
 }
