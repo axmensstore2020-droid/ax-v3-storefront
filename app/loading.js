1a5519a7d1adb94cd753f3070dab7b31edc4ce40
axmensstore2020-droid <axmensstore2020@gmail.com>
2026-09-22 23:47:43 +0400

    Remove root loading boundary to preserve 404 status codes

--- app/loading.js
+++
@@ -1,7 +0,0 @@
-import AXParticleLoader from '../components/AXParticleLoader';
-
-export default function Loading(){
-  return <div className="ax-route-loading" aria-busy="true">
-    <AXParticleLoader variant="route" label="Loading AX" />
-  </div>;
-}
